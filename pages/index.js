de3e1bcd79a443aaf8a83961e18df3cb29032e71
juansesu3 <juanseu3@hotmail.com>
2023-06-15 11:27:23 +0200

    strating with my admin app i'm going to implamate the next.auth

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="bg-blue-900 w-screen h-screen flex items-center">
+      <div className="text-center w-full">
+        <button className="bg-white p-2 px-4 rounded-lg">Login with Google</button>
+      </div>
+    </div>
+  );
+}
