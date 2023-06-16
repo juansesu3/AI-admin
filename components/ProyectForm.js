f3d98ee51e4e1c6c8343e111b840db7374837459
juansesu3 <juanseu3@hotmail.com>
2023-06-16 15:15:11 +0200

    I was created a button to upload the image

--- components/ProyectForm.js
+++ components/ProyectForm.js
@@ -8,6 +8,7 @@ const ProyectForm = ({
   description: existingDescription,
   linkCode: existingLinkCode,
   linkDeploy: existingLinkDeploy,
+  images,
 }) => {
   //states to storage my data proyect
   const [title, setTitle] = useState(existingTitle || "");
@@ -29,11 +30,9 @@ const ProyectForm = ({
     if (_id) {
       //update
       await axios.put("/api/proyects", { ...data, _id });
-      
     } else {
       //create
       await axios.post("/api/proyects", data);
-      
     }
     setGoToProyects(true);
   };
@@ -52,6 +51,28 @@ const ProyectForm = ({
         value={title}
         onChange={(ev) => setTitle(ev.target.value)}
       />
+      <label>Photos</label>
+      <div className="mb-2">
+        <label className="w-24 h-24 text-center flex flex-col items-center justify-center text-sm gap-1 text-gray-500 rounded-lg bg-gray-200">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5"
+            />
+          </svg>
+          <div>Upload</div>
+          <input type="file" className="hidden" />
+        </label>
+        {!images?.length && <div>No photos in this proyect</div>}
+      </div>
       <label>Proyect description</label>
       <textarea
         placeholder="proyect description"
