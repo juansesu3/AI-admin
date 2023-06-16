fa4c0318a42c396d7536f8b7bba639a65f83667b
juansesu3 <juanseu3@hotmail.com>
2023-06-16 16:55:48 +0200

    I was finished with AWS bucket now I can to upload my image to my bucket and obtein a link to consuming these images

--- pages/api/upload.js
+++ pages/api/upload.js
@@ -1,4 +1,9 @@
 import multiparty from "multiparty";
+import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import fs from "fs";
+import mime from "mime-types";
+
+const bucketName = "my-page-negiupp";
 
 const handle = async (req, res) => {
   const form = new multiparty.Form();
@@ -8,8 +13,34 @@ const handle = async (req, res) => {
       resolve({ fields, files });
     });
   });
-  console.log("length:", files.file.length);
-  console.log(files);
+  //console.log("length:", files.file.length);
+
+  const client = new S3Client({
+    region: "eu-north-1",
+    credentials: {
+      accessKeyId: process.env.S3_ACCESS_KEY,
+      secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+    },
+  });
+  const links = [];
+  for (const file of files.file) {
+    const ext = file.originalFilename.split(".").pop();
+    const newFilename = `${Date.now()}.${ext}`;
+    //console.log({ ext, file });
+    await client.send(
+      new PutObjectCommand({
+        Bucket: bucketName,
+        Key: newFilename,
+        Body: fs.readFileSync(file.path),
+        ACL: "public-read",
+        ContentType: mime.lookup(file.path),
+      }));
+
+      const link = `https://${bucketName}.s3.amazonaws.com/${newFilename}`;
+      links.push(link);
+  }
+
+  return res.json({links});
 };
 
 export const config = {
