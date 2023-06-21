5c28e82300bc8e8610a4171802ffbeb9352a2a39
juansesu3 <juanseu3@hotmail.com>
2023-06-21 14:38:23 +0200

    still working on responsive design hamburguer button is done

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,7 @@
 import Nav from "@/components/Nav";
 import { useSession, signIn, signOut } from "next-auth/react";
 import { useState } from "react";
+import Logo from "./Logo";
 
 const Layout = ({ children }) => {
   const [showNav, setShowNav] = useState(false);
@@ -22,20 +23,26 @@ const Layout = ({ children }) => {
 
   return (
     <div className="bg-bgGray min-h-screen ">
-      <button onClick={()=> setShowNav(true)}>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          fill="currentColor"
-          className="w-6 h-6"
-        >
-          <path
-            fillRule="evenodd"
-            d="M3 6.75A.75.75 0 013.75 6h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 6.75zM3 12a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 12zm0 5.25a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z"
-            clipRule="evenodd"
-          />
-        </svg>
-      </button>
+      <div className="block md:hidden flex items-center p-4">
+        <button onClick={() => setShowNav(true)}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              fillRule="evenodd"
+              d="M3 6.75A.75.75 0 013.75 6h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 6.75zM3 12a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 12zm0 5.25a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </button>
+        <div className="flex grow justify-center mr-6">
+          <Logo />
+        </div>
+      </div>
+
       <div className="flex">
         <Nav show={showNav} />
         <div className="bg-white flex-grow mt-2 mr-2 mb-2 rounded-lg p-4">
--- components/Nav.js
+++ components/Nav.js
@@ -1,6 +1,7 @@
 import { signOut } from "next-auth/react";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import Logo from "./Logo";
 
 const Nav = ({ show }) => {
   const inactiveLink = "flex gap-1 p-1";
@@ -23,23 +24,10 @@ const Nav = ({ show }) => {
         " top-0 text-gray-500 p-4 fixed w-full bg-bgGray h-full md:static md:w-auto transition-all"
       }
     >
-      <Link href={"/"} className="flex gap-1 mb-4 mr-4">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="w-6 h-6"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M12 21v-8.25M15.75 21v-8.25M8.25 21v-8.25M3 9l9-6 9 6m-1.5 12V10.332A48.36 48.36 0 0012 9.75c-2.551 0-5.056.2-7.5.582V21M3 21h18M12 6.75h.008v.008H12V6.75z"
-          />
-        </svg>
-        <span>MyAdminApp</span>
-      </Link>
+      <div className="mb-4 mr-4">
+        <Logo />
+      </div>
+
       <nav className="flex flex-col gap-2">
         <Link
           href={"/"}
