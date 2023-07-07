b12a4f2a4b0faca754ebe1774d11692dacf88ad6
juansesu3 <juanseu3@hotmail.com>
2023-07-07 23:34:38 +0200

    working on profile preparing data

---
+++ models/Profile.js
@@ -0,0 +1,14 @@
+import { Schema, model, models } from "mongoose";
+
+const ProfileSchema = new Schema({
+  name: { type: String, require: true },
+  greeting: { type: String, require: true },
+  shortIntro: { type: String, require: true },
+  introYourSelf: { type: String, require: true },
+  experinces: [{ type: String, require: true }],
+  education: [{ type: String, require: true }],
+  languages: { type: String, require: true },
+  skills: { type: String, require: true },
+});
+
+export const Profile = models.Profile || model("Profile", ProfileSchema);
