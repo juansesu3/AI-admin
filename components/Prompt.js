70c5acdca1268474e5230d25a60174af416af214
juansesu3 <juanseu3@hotmail.com>
2023-10-24 15:39:07 +0200

    FEATURE(Copie Prompt)

--- components/Prompt.js
+++ components/Prompt.js
@@ -1,13 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import Swal from "sweetalert2";
 
 const Prompt = () => {
   const numberStyle = "text-primary font-medium";
   const listStyle = "mb-2";
   const inputsPrompt = "w-auto h-6 text-center";
 
+  const [inputs, setInputs] = useState({
+    expertTopic: "",
+    websiteAbout: "",
+    userQuestion: "",
+    // ...other states
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setInputs((prevState) => ({ ...prevState, [name]: value }));
+  };
+
   const handleCopyClick = async () => {
     const promptDiv = document.getElementById("prompt");
     let textToCopy = "";
+
+    // Añadir valores de los inputs al principio del texto copiado
+    textToCopy += `Expert Topic: ${inputs.expertTopic}\n`;
+    textToCopy += `Website About: ${inputs.websiteAbout}\n`;
+    textToCopy += `User Question: ${inputs.userQuestion}\n`;
+    textToCopy += `Number of Words: ${inputs.numberWords}\n`;
+    textToCopy += `Main Keyword: ${inputs.mainKeyword}\n`;
+    textToCopy += `Secondary Keywords 1: ${inputs.secondaryKeywords1}\n`;
+    textToCopy += `Secondary Keywords 2: ${inputs.secondaryKeywords2}\n`;
+    textToCopy += `Secondary Keywords 3: ${inputs.secondaryKeywords3}\n`;
 
     // Iterar a través de cada elemento hijo del div
     promptDiv.childNodes.forEach((node) => {
@@ -42,57 +65,263 @@ const Prompt = () => {
       console.error("Error al copiar texto: ", err);
     }
   };
+
+  const handleSvgClick = (info) => {
+    Swal.fire({
+      title: "Information",
+      text: info,
+      icon: "info",
+      confirmButtonText: "Understood",
+    });
+  };
+
   return (
     <div
       id="prompt"
       className="bg-tableBg text-white p-4 shadow-lg rounded-md mb-8"
     >
       <h2 className="text-primary font-bold text-2xl mb-4">Prompt: </h2>
-      <div></div>
-      <p>
+
+      <div>
         Write like an SEO expert and{" "}
-        <input className={inputsPrompt} type="text" placeholder="xxxx" /> an
-        article for my{" "}
-        <input className={inputsPrompt} type="text" placeholder="xxxx" /> Follow
-        these guidelines:
-      </p>
+        <span className="inline-flex">
+          <input
+            name="expertTopic"
+            value={inputs.expertTopic}
+            onChange={handleInputChange}
+            className={inputsPrompt}
+            type="text"
+            placeholder="expert topic"
+          ></input>{" "}
+          <span
+            className="cursor-pointer  hover:text-primary"
+            onClick={() =>
+              handleSvgClick(
+                "Please enter the specific topic of your expertise on which you plan to write your article. For example, if you are an expert in machine learning, you could write 'Implementing Neural Networks in Python."
+              )
+            }
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z"
+              />
+            </svg>
+          </span>
+        </span>{" "}
+        an article for my{" "}
+        <span className="inline-flex">
+          <input
+            name="websiteAbout"
+            value={inputs.websiteAbout}
+            onChange={handleInputChange}
+            className={inputsPrompt}
+            type="text"
+            placeholder="what is your website about?"
+          />
+          <span
+            className="cursor-pointer hover:text-primary"
+            onClick={() =>
+              handleSvgClick(
+                "What is your website about? Please specify the core subject or niche you focus on. For example, if your website is about personal development, you could write 'Personal Development Techniques and Mindfulness Practices."
+              )
+            }
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z"
+              />
+            </svg>
+          </span>
+        </span>
+        Follow these guidelines:
+      </div>
       <ol>
         <li className={listStyle}>
           <span className={numberStyle}>1. </span>
           Address the search intent of a user who wants to know{" "}
-          <input className={inputsPrompt} type="text" placeholder="xxxx" />.
+          <span className="inline-flex ">
+            <input
+              name="userQuestion"
+              value={inputs.userQuestion}
+              onChange={handleInputChange}
+              className={inputsPrompt}
+              type="text"
+              placeholder="answer user question"
+            />
+            <span
+              className="cursor-pointer hover:text-primary"
+              onClick={() =>
+                handleSvgClick(
+                  "Please specify here the key topic or question that your website aims to address, to better align with the search intent of your users. For instance, if your website is focused on fitness, you could write 'How to achieve a balanced lifestyle through fitness and nutrition."
+                )
+              }
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z"
+                />
+              </svg>
+            </span>
+          </span>
+          .
         </li>
         <li className={listStyle}>
           <span className={numberStyle}>2. </span>
           The post should have a length of:{" "}
-          <input className={inputsPrompt} type="text" placeholder="xxxx" />
+          <span className="inline-flex">
+            <input
+              name="numberWords"
+              value={inputs.numberWords}
+              onChange={handleInputChange}
+              className={inputsPrompt}
+              type="number"
+              placeholder="number of words or length"
+            />
+            <span
+              className="cursor-pointer hover:text-primary"
+              onClick={() =>
+                handleSvgClick(
+                  "The post should have a length of: __ number of words or length in words. For example, if you're writing a comprehensive guide, aim for at least 2000 words; for a quick update, 500 words might be sufficient."
+                )
+              }
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z"
+                />
+              </svg>
+            </span>
+          </span>
           words.
         </li>
         <li className={listStyle}>
           <span className={numberStyle}>3. </span>
           The main keyword for SEO optimizations is{" "}
-          <input className={inputsPrompt} type="text" placeholder="xxxx" />.
+          <span className="inline-flex">
+            <input
+              name="mainKeyword"
+              value={inputs.mainKeyword}
+              onChange={handleInputChange}
+              className={inputsPrompt}
+              type="text"
+              placeholder="primary keywords for SEO"
+            />
+            <span
+              className="cursor-pointer hover:text-primary"
+              onClick={() =>
+                handleSvgClick(
+                  "The main keyword for SEO optimizations is ____. For example, if your article is about 'sustainable fashion,' your main keyword might be 'sustainable fashion trends'."
+                )
+              }
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z"
+                />
+              </svg>
+            </span>
+          </span>
+          .
         </li>
         <li className={listStyle}>
           <span className={numberStyle}>4. </span>
           The secondary keywords are{" "}
           <input
+            name="secondaryKeywords1"
+            value={inputs.secondaryKeywords1}
+            onChange={handleInputChange}
             className="w-auto h-6 text-center"
             type="text"
-            placeholder="xxxx"
+            placeholder="secondary keywords"
           />
           ,{" "}
           <input
+            name="secondaryKeywords2"
+            value={inputs.secondaryKeywords2}
+            onChange={handleInputChange}
             className="w-auto h-6 text-center"
             type="text"
-            placeholder="xxxx"
+            placeholder="secondary keywords"
           />
           , and{" "}
-          <input
-            className="w-auto h-6 text-center"
-            type="text"
-            placeholder="xxxx"
-          />
+          <span className="inline-flex">
+            <input
+              name="secondaryKeywords3"
+              value={inputs.secondaryKeywords3}
+              onChange={handleInputChange}
+              className="w-auto h-6 text-center"
+              type="text"
+              placeholder="secondary keywords"
+            />
+            <span
+              className="cursor-pointer hover:text-primary"
+              onClick={() =>
+                handleSvgClick(
+                  "The secondary keywords are ____. These are additional keywords that support your main keyword. For example, if your main keyword is 'sustainable fashion trends,' your secondary keywords could be 'eco-friendly clothing,' 'organic materials,' and 'ethical fashion brands."
+                )
+              }
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z"
+                />
+              </svg>
+            </span>
+          </span>
           .
         </li>
         <li className={listStyle}>
